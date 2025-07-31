8c9622422f4e49455f416cde0a859f1ae14a046a
Yash <iamyashsiwach@gmail.com>
2025-07-31 23:39:38 +0530

    update config.ts prod

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,21 +5,14 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseDriverOptions:
-    process.env.NODE_ENV === "production"
-      ? {
-          connection: {
-            ssl: {
-              rejectUnauthorized: false,
-            },
-          },
-        }
-      : {},
+      process.env.NODE_ENV === "production"
+        ? { connection: { ssl: { rejectUnauthorized: false } } }
+        : {},
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      authCors: process.env.AUTH_CORS,
+      storeCors: process.env.STORE_CORS,
+      adminCors: process.env.ADMIN_CORS,
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
   },
